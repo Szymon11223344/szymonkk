24d894a53848f975be2f51192586333c0fc07d92
3725185 <3725185@TE-GDA-D-D21E14>
2024-11-25 10:23:44 +0100

    Dodaj pliki projektów.

---
+++ szymonkk/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace szymonkk
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ szymonkk/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿namespace szymonkk
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            Random r = new Random();
+
+            int diceRoll = 6;
+
+            if (K4.IsChecked)
+            {
+                diceRoll = r.Next(1, 5);
+            }
+
+            if (K6.IsChecked)
+            {
+                diceRoll = r.Next(1, 7);
+            }
+
+            if (K10.IsChecked)
+            {
+                diceRoll = r.Next(1, 11);
+            }
+
+            if (K12.IsChecked)
+            {
+                diceRoll = r.Next(1, 13);
+            }
+
+            RollLabel.Text = "Wynik rzutu:   " + diceRoll.ToString();
+
+
+        }
+
+        private void CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            RadioButton rb = (RadioButton)sender;
+
+            int maxRoll = int.Parse((string)rb.Value);
+
+            string fileName = "k" + maxRoll + ".jpg";
+
+            bitcoin.Source = fileName;
+        }
+    }
+
+}
